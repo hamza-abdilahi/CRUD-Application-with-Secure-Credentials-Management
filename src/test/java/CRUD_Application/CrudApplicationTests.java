f7d5d2ff350f2459c309dbd15fdc123942d0ee84
Hamza Abdilahi <85694436+hamza-abdilahi@users.noreply.github.com>
2024-07-13 18:14:13 +0100

    first commit

---
+++ src/test/java/CRUD_Application/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package CRUD_Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
